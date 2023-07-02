64ff07e94805f1cdcee8daf5c2155511f6a1a897
HardikSutariya77 <129483369+HardikSutariya77@users.noreply.github.com>
2023-07-02 22:07:23 +0530

    final

--- EmployeeDetails/src/main/java/com/example/EmployeeDetails/EmployeeRepository/EmployeeRepository.java
+++ EmployeeDetails/src/main/java/com/example/EmployeeDetails/EmployeeRepository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.example.EmployeeDetails.EmployeeRepository;
 
 import com.example.EmployeeDetails.Model.Employee;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
--- EmployeeDetails/src/main/java/com/example/EmployeeDetails/EmployeeService/EmployeeService.java
+++ EmployeeDetails/src/main/java/com/example/EmployeeDetails/EmployeeService/EmployeeService.java
@@ -72,7 +72,30 @@ public class EmployeeService {
     }
 
     public void updateEmployeeData(Employee employee) {
+        /*getting old data from database*/
         Employee employee1=employeeRepository.findById(employee.getId()).orElse(null);
+        Employee employee2=employeeRepository.findByEmailId(employee.getEmailId());
+
+        /*check there should be email id for update data must be unique*/
+        /* case :1 if we want update data other than email than we just fetch data from database and get employee1 by
+                   searching on id and employee2 by searching on email id here both object is not null and id is same
+                   so if condition is not executed and data gets updated
+        */
+        /* case :2 if we want to update email id than we first check that email id is not occupied some other employee
+                   object
+                   --so first findById give object of employee1 old data
+                   --findByEmailId() give object for given Email-id which is we want to update
+                   --case:1 email id is unique or not occupied so employee2 is having null value and if condition is
+                     not executed we can use given email id
+                   --case:2 email id is occupied by some other data and already present in our data base that bring
+                     object that are already present in our database so it's employee2.getId() is not equal to employee1.getId()
+                     and if id is same than we try update same object which is have no issue
+         */
+
+        if(employee2!=null && employee1.getId()!=employee2.getId())
+            throw new EmployeeDataAlreadyExistsException("Employee Data with email id= "+employee.getEmailId()+" already present with Employee id= "+employee2.getId());
+
+
         if(employee1!=null) {
             employee1.setDepartment(employee.getDepartment());
             employee1.setName(employee.getName());
